4e09dc28c3d0c0a7f869610563676f7d88c49995
Donald Fellow <Donald.Fellow@aa.com>
2018-10-12 01:27:22 -0700

    version 0.3.0

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 8080
 app.get('/jworg-newsroom.json', (req, res) => 
 Feed.load('https://www.jw.org/en/news/jw/rss/NewsSubsectionRSSFeed/feed.xml', function(err, rss){
 	res.setHeader('Content-Type', 'application/json');
-    res.send(rss);
+    res.type('json');
+	res.json(rss);
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
