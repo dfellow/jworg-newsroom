0a7bd17aeba08c9ae48537c76e8c2dbc36f2b218
dfellow <dfellow@users.noreply.github.com>
2018-10-12 11:34:01 -0700

    update

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var Feed = require('rss-to-json');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
 
 app.get('/jworg-newsroom.json', (req, res) => 
 Feed.load('https://www.jw.org/en/news/jw/rss/NewsSubsectionRSSFeed/feed.xml', function(err, rss){
-	res.setHeader('Content-Type', 'application/json');
+	//res.setHeader('Content-Type', 'application/json');
     res.type('json');
-	res.json(rss);
+	res.send(rss);
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
