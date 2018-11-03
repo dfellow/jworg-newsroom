950ebe3178c7fb46cd760e2cd4b3e435c743ae4e
Donald Fellow <Donald.Fellow@aa.com>
2018-11-03 10:13:26 -0700

    Add WhatsNew

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Feed = require('rss-to-json');
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
 
 app.get('/jworg-newsroom.json', (req, res) => 
@@ -10,6 +10,14 @@ Feed.load('https://www.jw.org/en/news/jw/rss/NewsSubsectionRSSFeed/feed.xml', fu
     res.type('json');
 	res.send(rss);
 }));
+
+app.get('/jworg-whatsnew.json', (req, res) => 
+Feed.load('https://www.jw.org/en/whats-new/rss/WhatsNewWebArticles/feed.xml', function(err, rss){
+	//res.setHeader('Content-Type', 'application/json');
+    res.type('json');
+	res.send(rss);
+}));
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
